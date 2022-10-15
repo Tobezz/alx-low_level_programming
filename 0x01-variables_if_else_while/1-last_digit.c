7ae3270fb54d2051e40f1b765fb1fcc420a00187
Tobezz <johntobez@gmail.com>
2022-10-15 12:46:15 +0000

    task 1 commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry
  *
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
@@ -21,7 +21,7 @@ int main(void)
 	}
 	else if (a == 0)
 	{
-		printf("Last digit of %d is %d and is o\n", n, a);
+		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
 	else
 	{
