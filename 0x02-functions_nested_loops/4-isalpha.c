138c20e2953870a85a0b18b49c48680ad482fe64
Tobezz <johntobez@gmail.com>
2022-10-19 21:40:35 +0000

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,17 +4,12 @@
  * _isalpha - checks for alphabets
  * @c: character to be checked
  *
- * Return: 1if character is a letter,0notherwise
+ * Return: 1 if character is a letter,0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && C <= 90))
-	{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && C <= 'Z'))
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-	_putchar('\n');
 }
