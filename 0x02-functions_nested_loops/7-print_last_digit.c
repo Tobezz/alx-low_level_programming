43a39fd4c2a126a8be49b483342e2a1a22ae46bb
Tobezz <johntobez@gmail.com>
2022-10-19 22:39:21 +0000

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,9 @@ int print_last_digit(int n)
 	int ld = n % 10;
 
 	if (ld < 0)
-	{
-		_putchar(-1 + 48);
-		return (-1);
-	}
-	else
-	{
-		_putchar(-1 + 48);
-		return (-1);
-	}
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (ld);
 }
