0801f2aa4c0e1cc7c799362a6f32a285082b5298
Tobezz <johntobez@gmail.com>
2022-10-23 10:20:54 +0000

    commit changes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * times_table - Prints the times table of the input,
+ * print_times_table - Prints the times table of the input,
  * starting with 0.
  * @n: The value of the times table to be printed.
+ * return: 0
  */
 void print_times_table(int n)
 {
